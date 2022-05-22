f5a23b627ea9c8425cfa149fd7d74d2578fd3b93
omar-faruk11 <omarabdullah1811@gmail.com>
2022-05-22 12:32:09 +0600

    Parts section added

---
+++ src/Pages/Home/Part.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Part = ({ part }) => {
+    const { name, picture, description, min_order, available, price } = part;
+    return (
+        <div>
+            <div class="card card-compact h-full bg-base-100 shadow-xl">
+                <figure><img src={picture} alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{name}</h2>
+                    <p className='text-gray-500'>{description.slice(0,100)}</p>
+                    <p className=' text-base capitalize '>minimum orders: {min_order}</p>
+                    <p className=' text-base capitalize '>available: {available}</p>
+                    <p className=' text-base capitalize '> price: {price}</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn w-full">book now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Part;
--- src/Pages/Home/Parts.jsx
+++ src/Pages/Home/Parts.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Part from './Part';
 
 const Parts = () => {
+    const [parts , setParts] = useState([]);
+    useEffect(()=>{
+        (async()=>{
+            const {data} = await axios.get('parts.json');
+            setParts(data);
+            console.log(data);
+        })()
+    },[])
+    
     return (
-        <div>
-            parts
-        </div>
+        <section className=' container mx-auto my-16'>
+            <h2 className=' text-center text-3xl font-semibold mb-8 text-rose-500 '>Parts</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-5">
+            {
+                parts.map((part, index) =><Part key={index} part={part}/>)
+            }
+            </div>
+        </section>
     );
 };
 
 export default Parts;
+
+
+/* 
+minimumorderquantity
+*/
