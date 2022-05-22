74cc43329a623cd011b0ded43ae583b8e6873d79
omar-faruk11 <omarabdullah1811@gmail.com>
2022-05-22 10:50:18 +0600

    Banner section added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            bnanner
-        </div>
+        <section class="hero min-h-screen" style={{ backgroundImage: "url(https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200)" }}>
+            <div class="hero-overlay "></div>
+            <div class="hero-content text-center text-white">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">Best Quality Auto Parts</h1>
+                    <p class="mb-5">Let us help you find what you need.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </section>
+
     );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
+import Parts from './Parts';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div>
-            home
+            <Banner/>
+            <Parts/>
+            <Reviews/>
+            <BusinessSummary/>
         </div>
     );
 };
